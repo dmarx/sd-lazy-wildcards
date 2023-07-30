d6849364cc56f8032bde2f7711b45b05c0e1e16f
David Marx <david.marx84@gmail.com>
2023-07-29 19:36:43 -0700

    Update install.py

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
 import launch
 
 for lib in "openai loguru".split():
-if not launch.is_installed(lib):
-    launch.run_pip(f"install {lib}", "requirement for Lazy Wildcard")
+    if not launch.is_installed(lib):
+        launch.run_pip(f"install {lib}", "requirement for Lazy Wildcard")
